45e7ae90acc336d6f4dfc1e01613043d5f409819
Nuno Durão <nunofdurao@gmail.com>
2023-04-24 11:32:16 +0100

    add pic

--- src/index.js
+++ src/index.js
@@ -4,36 +4,4 @@ const myCanvas = document.getElementById('canvas');
 const ctx = myCanvas.getContext('2d');
 
 const img = new Image();
-img.src ="../images/8-bit-mario-background-1-Background-Download.png"
-
-const backgroundImage = {
-  img: img,
-  x: 0,
-  speed: -1,
-
-  move: function() {
-    this.x += this.speed;
-    this.x %= myCanvas.width;
-  },
-
-  draw: function() {
-    ctx.drawImage(this.img, this.x, 0);
-    if (this.speed < 0) {
-      ctx.drawImage(this.img, this.x + canvas.width, 0);
-    } else {
-      ctx.drawImage(this.img, this.x - this.img.width, 0);
-    }
-  },
-};
-
-function updateCanvas() {
-  backgroundImage.move();
-
-  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-  backgroundImage.draw();
-
-  requestAnimationFrame(updateCanvas);
-}
-
-
-img.onload = updateCanvas;
+img.src =""
